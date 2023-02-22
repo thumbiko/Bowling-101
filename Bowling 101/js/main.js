197f03d278754331c92275ad1079132a3749e0c6
biko sulu <c19713071@mytudublin.ie>
2023-02-22 02:42:55 +0000

    improvements

--- Bowling 101/js/main.js
+++ Bowling 101/js/main.js
@@ -40,14 +40,6 @@
     });
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        items: 1,
-        dots: true,
-        loop: true,
-    });
 
     
 })(jQuery);
